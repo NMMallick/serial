4ddda289c74f024d5a5719496e3b46d81c310c98
Nathaniel Mallick <nmm109@pitt.edu>
2023-08-27 14:48:25 -0400

    added some more descriptive comments to the example program

--- examples/test.cc
+++ examples/test.cc
@@ -1,19 +1,28 @@
+/**
+ *
+ */
+
+// For std::cout
 #include <iostream>
 
+// Include for the serial library
 #include <serial/Serial.hh>
 
 int main(int argc, char **argv)
 {
+    // Create a custom POD datatype
     struct MyStruct {
 	float one,
-	    two,
+	    two_point_one,
 	    three,
 	    four;
     };
-
-    MyStruct data = { .one=1.0, .two=2.1, .three=3.0, .four=4.0};
+    MyStruct data = { .one=1.0, .two_point_one=2.1, .three=3.0, .four=4.0};
 
     // Instance the serial interface object
+    //
+    // @note This will fail if "/dev/ttyACM0" device file does not exist
+    //        or if the device file is already open
     serial::Serial serial("/dev/ttyACM0");
 
     // Create a unique pointer to a thread safe buffer object
@@ -26,12 +35,12 @@ int main(int argc, char **argv)
     serial.write(buffer.get());
     serial.read(buffer.get());
 
-    // Get the data returned from read()
+    // Get the data returned from reading the serial buffer
     const auto ret = buffer->getData();
 
     // Go on about your life <(*.*<)
     std::cout << "one : " << ret.one << std::endl
-	      << "two point one : " << ret.two << std::endl
+	      << "two point one : " << ret.two_point_one << std::endl
 	      << "three : " << ret.three << std::endl
 	      << "four : " << ret.four << std::endl;
 
